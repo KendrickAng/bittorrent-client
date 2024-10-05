8e962acd2fdffe705977fc958ec0cb38c02334cf
Kendrick <kendrick.awh@gmail.com>
2024-10-06 01:27:52 +0800

    Completed single-threaded download of the entire file from a single client.
    Also remove concurrency from existing approach.

--- pkg/trackerprotocol/downloadmanager.go
+++ pkg/trackerprotocol/downloadmanager.go
@@ -40,46 +40,25 @@ type pieceResult struct {
 
 func NewDownloadManager(torrent *bencodeutil.SimpleTorrentFile, clients []*Client) (*DownloadManager, error) {
 	return &DownloadManager{
-		torrent:            torrent,
-		clients:            clients,
-		reconstructer:      NewReconstructer(torrent.PieceHashes),
-		pieceResultChannel: make(chan *pieceResult, maxResultThreads),
-		done:               make(chan struct{}),
+		torrent:       torrent,
+		clients:       clients,
+		reconstructer: NewReconstructer(torrent.PieceHashes),
+		done:          make(chan struct{}),
 	}, nil
 }
 
 func (d *DownloadManager) Start(ctx context.Context) error {
 	// split pieces into pieces of work
 	downloadTasks := createDownloadTasks(d.torrent)
-	pieceDownloadChannel := make(chan *pieceRequest, len(downloadTasks))
-	for _, task := range downloadTasks {
-		pieceDownloadChannel <- &task
-	}
-	defer close(pieceDownloadChannel)
 
-	// TODO for now just use one goroutine
+	// TODO make this multi-threaded and download from multiple clients
 	// handle with clients
-	worker := NewDownloadWorker(d.clients[0], pieceDownloadChannel, d.pieceResultChannel)
-	if err := worker.Start(ctx); err != nil {
+	results, err := NewDownloadWorker(d.clients[0]).Start(ctx, downloadTasks)
+	if err != nil {
 		return err
 	}
 
-	// Create one handler per peer
-	//for _, peer := range d.clients {
-	//	go func(peer *Client, requests chan *pieceRequest, results chan *pieceResult) {
-	//		worker := NewDownloadWorker(peer, requests, results)
-	//		if err := worker.Start(ctx); err != nil {
-	//			println("worker ", peer.String(), " error ", err.Error())
-	//		}
-	//	}(peer, pieceDownloadChannel, d.pieceResultChannel)
-	//}
-
 	// continue until user cancels or download completes
-	var results []*pieceResult
-	for result := range d.pieceResultChannel {
-		results = append(results, result)
-	}
-
 	final, err := d.reconstructer.Reconstruct(results)
 	if err != nil {
 		return err
@@ -93,10 +72,16 @@ func createDownloadTasks(torrent *bencodeutil.SimpleTorrentFile) []pieceRequest 
 	var downloadTasks []pieceRequest
 
 	for i, pieceHash := range torrent.PieceHashes {
+		// Last piece may be smaller than piece length
+		pieceLength := torrent.PieceLength
+		if i == len(torrent.PieceHashes)-1 {
+			pieceLength = torrent.Length % torrent.PieceLength
+		}
+
 		downloadTasks = append(downloadTasks, pieceRequest{
 			pieceIndex:        i,
 			requestLength:     maxRequestLength,
-			pieceLength:       torrent.PieceLength,
+			pieceLength:       pieceLength,
 			expectedPieceHash: pieceHash,
 		})
 	}
--- pkg/trackerprotocol/downloadworker.go
+++ pkg/trackerprotocol/downloadworker.go
@@ -10,54 +10,54 @@ import (
 // DownloadWorker handles the download of a single piece of data in the torrent.
 // A torrent is split into many pieces for download.
 type DownloadWorker struct {
-	client   *Client
-	requests chan *pieceRequest
-	results  chan *pieceResult
+	client *Client
 }
 
-func NewDownloadWorker(client *Client, requests chan *pieceRequest, results chan *pieceResult) *DownloadWorker {
+func NewDownloadWorker(client *Client) *DownloadWorker {
 	return &DownloadWorker{
-		client:   client,
-		requests: requests,
-		results:  results,
+		client: client,
 	}
 }
 
-// TODO: refactor this into a state machine e.g. switch statement
 // Start starts the worker downloading available pieces from a client.
-func (d *DownloadWorker) Start(ctx context.Context) error {
-	for req := range d.requests {
+func (d *DownloadWorker) Start(ctx context.Context, requests []pieceRequest) ([]*pieceResult, error) {
+	pieceResults := make([]*pieceResult, len(requests))
+
+	for i, req := range requests {
+		if ctx.Err() != nil {
+			return nil, ctx.Err()
+		}
+
 		// If choked, try to unchoke ourselves
 		if d.client.IsChoked() {
 			if err := d.client.SendInterestedMessage(); err != nil {
-				d.handleError(err, req)
-				continue
+				return nil, err
 			}
 			if _, err := d.client.ReceiveUnchokeMessage(); err != nil {
-				d.handleError(err, req)
-				continue
+				return nil, err
 			}
 			d.client.SetChoked(false)
 			println(d.client.String(), "unchoked")
 		}
 
+		remainingBytes := req.pieceLength
 		numRequests := int(math.Ceil(float64(req.pieceLength) / float64(req.requestLength)))
 		index := uint32(req.pieceIndex)
 		blocks := make([][]byte, numRequests)
-		//reqLength := uint32(req.pieceLength) TODO for some reason using this makes it not work.
 
 		for i := 0; i < numRequests; i++ {
 			begin := uint32(req.requestLength * i)
+			reqLength := uint32(min(req.requestLength, remainingBytes))
 
-			if err := d.client.SendRequestMessage(index, begin, maxRequestLength); err != nil {
-				return err
+			if err := d.client.SendRequestMessage(index, begin, reqLength); err != nil {
+				return nil, err
 			}
 
 		Inner:
 			for {
 				msg, err := d.client.ReceiveMessage()
 				if err != nil {
-					return err
+					return nil, err
 				}
 				switch msg.ID {
 				case MsgKeepAlive:
@@ -72,9 +72,10 @@ func (d *DownloadWorker) Start(ctx context.Context) error {
 					piece := msg.AsMsgPiece()
 					println("piece", index, ":", begin, "of length", len(piece.Block), "of total", req.pieceLength)
 					if piece.Begin != begin || piece.Index != index {
-						return errors.New("invalid piece")
+						return nil, errors.New("invalid piece")
 					}
 					blocks[i] = piece.Block
+					remainingBytes -= len(piece.Block)
 					break Inner
 				default:
 					panic(msg.ID)
@@ -82,49 +83,17 @@ func (d *DownloadWorker) Start(ctx context.Context) error {
 			}
 		}
 
-		// Unchoked, start downloading pieces
-		//blocks := make([][]byte, numRequests)
-		//for i := 0; i < numRequests; i++ {
-		//	// Last request may be smaller than the others
-		//	if i == numRequests-1 {
-		//		// TODO
-		//	}
-		//	index := uint32(req.pieceLength)
-		//	begin := uint32(req.requestLength * i)
-		//	length := uint32(req.pieceLength)
-		//	if err := d.client.SendRequestMessage(index, begin, length); err != nil {
-		//		d.handleError(err, req)
-		//		continue
-		//	}
-		//	pieceMsg, err := d.client.ReceivePieceMessage()
-		//	if err != nil {
-		//		d.handleError(err, req)
-		//		continue
-		//	}
-		//	println(d.client.String(), " received piece message")
-		//	if pieceMsg.Begin != begin || pieceMsg.Index != index {
-		//		d.handleError(errors.New("invalid piece"), req)
-		//		continue
-		//	}
-		//	blocks[i] = pieceMsg.Block
-		//}
-
 		var finalBlocks []byte
 		for _, block := range blocks {
 			finalBlocks = append(finalBlocks, block...)
 		}
 
-		d.results <- &pieceResult{
+		pieceResults[i] = &pieceResult{
 			piece: finalBlocks,
 			index: req.pieceIndex,
 			hash:  hashutil.BTHash(finalBlocks),
 		}
 	}
 
-	return nil
-}
-
-func (d *DownloadWorker) handleError(err error, req *pieceRequest) {
-	println("worker ", d.client.String(), " error ", err.Error())
-	d.requests <- req
+	return pieceResults, nil
 }
